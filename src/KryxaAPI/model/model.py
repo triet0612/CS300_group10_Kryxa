0ac5e986481be09a6a24daaf3fbcd1f15686a3ed
duckhiem2003 <100983194+duckhiem2003@users.noreply.github.com>
2023-10-22 23:27:34 +0700

    change password constraint again
    change le = 3 to max_length = 3

--- src/KryxaAPI/model/model.py
+++ src/KryxaAPI/model/model.py
@@ -15,7 +15,7 @@ class Bill(BaseModel):
 
 class Pc(BaseModel):
     PcID: Annotated[int, Field(ge=0)]
-    Password: Annotated[str, Field(le=3)]
+    Password: Annotated[str, Field(max_length=3)]
     MAC: str
     IPv4: str
     TimeUsage: Annotated[int, Field(default=0)]

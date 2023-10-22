e582703dfd2f15e5b3a4de0338a7ce13d34cd522
duckhiem2003 <100983194+duckhiem2003@users.noreply.github.com>
2023-10-22 22:13:31 +0700

    fix constraint of PC class
    change password: e=3 to le=3

--- src/KryxaAPI/model/model.py
+++ src/KryxaAPI/model/model.py
@@ -15,7 +15,7 @@ class Bill(BaseModel):
 
 class Pc(BaseModel):
     PcID: Annotated[int, Field(ge=0)]
-    Password: Annotated[str, Field(e=3)]
+    Password: Annotated[str, Field(le=3)]
     MAC: str
     IPv4: str
     TimeUsage: Annotated[int, Field(default=0)]

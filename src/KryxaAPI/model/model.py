18a1b98ac4a9f5aec4b130de8368c56bcf430708
Tr1ck4 <91774874+Tr1ck4@users.noreply.github.com>
2023-10-22 20:45:25 +0700

    Sale item module
    SaleItem class created

--- src/KryxaAPI/model/model.py
+++ src/KryxaAPI/model/model.py
@@ -11,3 +11,11 @@ class Bill(BaseModel):
     Note: Annotated[str, Field(max_length=100)]
     Total: Annotated[float, Field(default=0)]
     Cart: list[dict]
+
+class SaleItems(BaseModel):
+    ItemID: Annotated[int, Field(ge = 0)]
+    Name: Annotated[str, Field(max_length=20)]
+    Price: Annotated[float, Field(ge=1000)]
+    Category: Annotated[str, Field(max_length=20)]
+    ItemStatus: Literal['Deprecated','On Sale']
+    Stock: Annotated[int, Field(ge = 0)]

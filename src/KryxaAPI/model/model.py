31158f0a728bb9b521313821bb1574447d2c237d
duckhiem2003 <100983194+duckhiem2003@users.noreply.github.com>
2023-10-22 22:07:49 +0700

    first version of PC model

--- src/KryxaAPI/model/model.py
+++ src/KryxaAPI/model/model.py
@@ -11,3 +11,14 @@ class Bill(BaseModel):
     Note: Annotated[str, Field(max_length=100)]
     Total: Annotated[float, Field(default=0)]
     Cart: list[dict]
+
+
+class Pc(BaseModel):
+    PcID: Annotated[int, Field(ge=0)]
+    Password: Annotated[str, Field(e=3)]
+    MAC: str
+    IPv4: str
+    TimeUsage: Annotated[int, Field(default=0)]
+
+
+

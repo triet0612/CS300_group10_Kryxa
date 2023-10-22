ce6d9a1579791430b05f231d50e379e235791ca5
triet0612 <triet0612@gmail.com>
2023-10-22 10:58:33 +0700

    Revert "Merge pull request #5 from triet0612/minh"
    This reverts commit 87fce32994418a021a94eb158768c1242983c51a, reversing
    changes made to 764baaa5bc244e4ba6731eb28af9642796de0f9e.

--- docs/analysis and design/design.sql
+++ docs/analysis and design/design.sql
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-CREATE TABLE PC(
-    PCID INTEGER,
-    Password TEXT,
-    MAC TEXT,
-    time_usage INTEGER,
-    PRIMARY KEY(PCID)
-);
-
-CREATE TABLE Admin(
-    AdminID INTEGER,
-    Fullname TEXT,
-    Phone TEXT,
-    Password TEXT,
-    PRIMARY KEY(AdminID)
-);
-
-CREATE TABLE Item(
-    ItemID INTEGER,
-    Name TEXT,
-    Price REAL,
-    Category TEXT,
-    PRIMARY KEY(ItemID)
-);
-
-CREATE TABLE Bill(
-    PCID INTEGER,
-    bill_datetime TEXT,
-    Status INTEGER DEFAULT 0,
-    AdminID INTEGER,
-	PRIMARY KEY(PCID, bill_datetime)
-    FOREIGN KEY(PCID) REFERENCES PC(PCID)
-    FOREIGN KEY(AdminID) REFERENCES Admin(AdminID)
-);
-
-CREATE TABLE Cart(
-    PCID INTEGER,
-    Cart_datetime TEXT,
-    ItemID INTEGER,
-    Amount INTEGER DEFAULT 0,
-	PRIMARY KEY(PCID, Cart_datetime)
-    FOREIGN KEY(PCID) REFERENCES Bill(PCID)
-    FOREIGN KEY(Cart_datetime) REFERENCES Bill(bill_datetime)
-    FOREIGN KEY(ItemID) REFERENCES Item(ItemID)
-)
-
-ALTER TABLE Bill ADD COLUMN NOTE TEXT DEFAULT "No note"
-=======
 CREATE TABLE "Admin" (
     "AdminID" INTEGER NOT NULL CHECK("AdminID" >= 0) UNIQUE,
     "Fullname" TEXT NOT NULL,
@@ -82,4 +34,3 @@ CREATE TABLE "Bill" (
     FOREIGN KEY("AdminID") REFERENCES "Admin"("AdminID"),
     PRIMARY KEY("PcID","Datetime")
 );
->>>>>>> main

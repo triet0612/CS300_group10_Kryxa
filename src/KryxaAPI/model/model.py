8b0a3da22b40a55f089f7bd420eb1ccc7db2e26d
duckhiem2003 <100983194+duckhiem2003@users.noreply.github.com>
2023-10-23 21:55:50 +0700

    change constraint of Password in PC

--- src/KryxaAPI/model/model.py
+++ src/KryxaAPI/model/model.py
@@ -16,7 +16,7 @@ class Bill(BaseModel):
 
 class Pc(BaseModel):
     PcID: Annotated[int, Field(ge=0)]
-    Password: Annotated[str, Field(max_length=3)]
+    Password: Annotated[str, Field(max_length=3, min_length=3)]
     MAC: str
     IPv4: str
     TimeUsage: Annotated[int, Field(default=0)]

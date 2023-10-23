44c9ec445e3626f421e07ad4299239d088fcd46c
BuiVuBaoMinh <buivubaominhlqd@gmail.com>
2023-10-23 09:32:16 +0700

    Add Admin model

--- src/KryxaAPI/model/model.py
+++ src/KryxaAPI/model/model.py
@@ -11,3 +11,10 @@ class Bill(BaseModel):
     Note: Annotated[str, Field(max_length=100)]
     Total: Annotated[float, Field(default=0)]
     Cart: list[dict]
+
+
+class Admin(BaseModel):
+    AdminID: Annotated[int, Field(ge=0)]
+    Fullname: Annotated[str, Field(max_length=100, min_length=1)]
+    Phone: Annotated[str, Field(min_length=1)]
+    Password: Annotated[str, Field(min_length=1)]

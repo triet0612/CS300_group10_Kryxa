5f1a3a2684ca42f302c20f68d1bfda2aeb65775c
triet0612 <triet0612@gmail.com>
2023-10-28 10:01:41 +0700

    small change

--- src/KryxaAPI/model/SaleItems.py
+++ src/KryxaAPI/model/SaleItems.py
@@ -1,10 +1,11 @@
-
 from pydantic import BaseModel, Field
 from typing import Annotated, Literal
+
+
 class SaleItems(BaseModel):
-    ItemID: Annotated[int, Field(ge = 0)]
+    ItemID: Annotated[int, Field(ge=0)]
     Name: Annotated[str, Field(max_length=20)]
     Price: float
     Category: Annotated[str, Field(max_length=20)]
-    ItemStatus: Literal['Deprecated','On Sale']
-    Stock: Annotated[int, Field(ge = 0)]
+    ItemStatus: Literal['Deprecated', 'On Sale']
+    Stock: Annotated[int, Field(ge=0)]

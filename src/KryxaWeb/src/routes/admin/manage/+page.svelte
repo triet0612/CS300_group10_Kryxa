c0f79d937d8d4f87f168b6c44931a6538e7d0ea1
Tr1ck4 <91774874+Tr1ck4@users.noreply.github.com>
2023-10-28 09:56:54 +0700

    create file items

---
+++ src/KryxaWeb/src/routes/admin/manage/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import background from '$lib/assets/admin/nav-login-bg.png';
+</script>
+
+<div id="back" class="h-screen relative" style="background-image: url({background});">
+    <div class="grid bg-amber-500 p-5 w-3/12 h-60 rounded-xl place-items-center justify-center relative top-1/3 left-2/4">
+
+    </div>
+</div>
+
+<style>
+    #back {
+        background-size: 100% 100%;
+    }
+</style>

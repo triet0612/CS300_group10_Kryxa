23c847e7d222b1cabbba0cb71f7408d84017c48d
BuiVuBaoMinh <buivubaominhlqd@gmail.com>
2023-10-28 09:57:30 +0700

    seperate Admin class from model.py

--- src/KryxaAPI/model/model.py
+++ src/KryxaAPI/model/model.py
@@ -10,13 +10,6 @@ class Pc(BaseModel):
     TimeUsage: Annotated[int, Field(default=0)]
 
 
-class Admin(BaseModel):
-    AdminID: Annotated[int, Field(ge=0)]
-    Fullname: Annotated[str, Field(max_length=100, min_length=1)]
-    Phone: Annotated[str, Field(min_length=1)]
-    Password: Annotated[str, Field(min_length=1)]
-
-
 class SaleItems(BaseModel):
     ItemID: Annotated[int, Field(ge=0)]
     Name: Annotated[str, Field(max_length=20)]

33f270b9841b4b6622fb4a5e253a7f6962311177
duckhiem2003 <100983194+duckhiem2003@users.noreply.github.com>
2023-10-28 14:36:19 +0700

    edit default endpoint of admin

--- src/KryxaAPI/router/admin.py
+++ src/KryxaAPI/router/admin.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, HTTPException, Response, Depends
 from typing import Annotated
 from auth import checkAdminAccount, generate_token, validateAdminToken, AccountDTO
-
+import array as arr
 adminRouter = APIRouter(tags=["admin"])
 
 
-@adminRouter.get("/")
-async def home_admin(acc: Annotated[AccountDTO, Depends(validateAdminToken)]):
-    return {"message": "welcome admin ID: " + str(acc.ID)}
+# @adminRouter.get("/")
+# async def home_admin(acc: Annotated[AccountDTO, Depends(validateAdminToken)]):
+#     return {"message": "welcome admin ID: " + str(acc.ID)}
+
 
 
 @adminRouter.post("/login")
@@ -21,3 +22,5 @@ async def login(acc: AccountDTO, res: Response) -> str:
     except Exception as err:
         print(err)
         raise HTTPException(status_code=401, detail="Error validating")
+
+

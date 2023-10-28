27cbedcffe4763f515429020b5f33f329a3b9df7
BuiVuBaoMinh <buivubaominhlqd@gmail.com>
2023-10-28 11:52:12 +0700

    [build] Add a prototype of get_all_items() to admin.py
      Add fake_items_db as testing SaleItem List.
    
      Add prototype of API function get_all_items() to admin.py for retrieving all items from database.
    
      get_all_items() current behavior:
        - Check if SaleItem List is empty, raise 404/"No items" it is.
        - Return fake_item_db.
    
      Expected improvements:
        - Implement proper SaleItem List retrival function.
        - Change return List to itemList (the proper one).
        - Consider Exceptions' codes and details.

--- src/KryxaAPI/router/admin.py
+++ src/KryxaAPI/router/admin.py
@@ -4,6 +4,46 @@ from auth import checkAdminAccount, generate_token, validateAdminToken, AccountD
 
 adminRouter = APIRouter(tags=["admin"])
 
+fake_items_db = [
+    {
+        "ItemID": "1",
+        "Name": "Com ga",
+        "Price": "50000",
+        "Category": "Food",
+        "ItemStatus": "On sale",
+        "Stock": "1"
+    },
+    {
+        "ItemID": "2",
+        "Name": "Com khong ga",
+        "Price": "50000",
+        "Category": "Food",
+        "ItemStatus": "On sale",
+        "Stock": "1"
+    }, {
+        "ItemID": "3",
+        "Name": "Com ga khong",
+        "Price": "50000",
+        "Category": "Food",
+        "ItemStatus": "On sale",
+        "Stock": "1"
+    }, {
+        "ItemID": "4",
+        "Name": "Pepsi Lon 500ml",
+        "Price": "9000",
+        "Category": "Drink",
+        "ItemStatus": "On sale",
+        "Stock": "1"
+    }, {
+        "ItemID": "5",
+        "Name": "Revive 500ml",
+        "Price": "10000",
+        "Category": "Drink",
+        "ItemStatus": "On sale",
+        "Stock": "1"
+    }
+]
+
 
 @adminRouter.get("/")
 async def home_admin(acc: Annotated[AccountDTO, Depends(validateAdminToken)]):
@@ -21,3 +61,24 @@ async def login(acc: AccountDTO, res: Response) -> str:
     except Exception as err:
         print(err)
         raise HTTPException(status_code=401, detail="Error validating")
+
+
+# TODO: getAllItem()
+@adminRouter.get("/items")
+async def get_all_items():
+    try:
+        # TODO: replace fake_items_db with itemList
+        # itemList = List of items get from getAllItem()
+        if len(fake_items_db) == 0:
+            raise HTTPException(status_code=404,
+                                detail="No items")  # This should not be 404, should have a notification screen
+        return fake_items_db
+    except Exception as err:
+        print(err)
+
+
+# TODO: getItemByID(ID: int)
+
+# TODO: getItemsByCategory(category: str)
+
+# TODO: getItemsByName(nameString: str)

87b8d2441dec8d108c3b030f5deeb8d3e9df3aad
triet0612 <triet0612@gmail.com>
2023-10-31 20:31:04 +0700

    nav bar changes

--- src/KryxaWeb/src/lib/components/AdminNav.svelte
+++ src/KryxaWeb/src/lib/components/AdminNav.svelte
@@ -3,25 +3,22 @@
     const items = ["Management", "Items", "Tournament", "Statistics", "Settings"]
 </script>
 
-<aside class="fixed top-0 left-0 w-64 h-screen">
-    <div class="h-full px-3 overflow-y-auto bg-[#2B2A2A]">
-        <ul class="text-xl text-[#E3A052]">
+<aside class="top-0 left-0 w-64 m-5 p-2 h-screen flex flex-col justify-center">
+    <div class="px-3 py-5 rounded-xl overflow-y-auto bg-gradient-to-b from-black to-[#352900] border-[1px] border-white">
+        <ul class="text-2xl text-[#E3A052] font-BlackOpsOne">
             <li><img src="{NavIcons["Logo"]}" alt="Logo"></li>
             {#each items as it}
                 <div class="flex m-3">
-                    <li class="flex items-center p-2">
-                        <div class="w-10">
-                            <img src="{NavIcons[it]}" alt="Icon">
-                        </div>
-                        <div>
-                            <a href="/admin/{it}">
-                                <p class="ml-6">{it}</p>
-                            </a>
-                        </div>
+                    <li class="flex p-2 hover:bg-[#E3A052] hover:text-black">
+                        <a href="/admin/{it}">{it}</a>
                     </li>
                 </div>
-                <hr>
             {/each}
+            <div class="flex mt-5 border-[1px] hover:bg-[#E3A052] hover:text-black justify-center">
+                <li class="flex items-center justify-center p-2">
+                    <button>Logout</button>
+                </li>
+            </div>
         </ul>
     </div>
 </aside>

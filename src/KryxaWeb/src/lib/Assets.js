5469df397efaf3f71ae41471fcfbdb412c2ed19d
LucidNg <94208867+LucidNg@users.noreply.github.com>
2023-11-02 15:44:28 +0700

    view list pc (no database fetch)

--- src/KryxaWeb/src/lib/Assets.js
+++ src/KryxaWeb/src/lib/Assets.js
@@ -1,8 +1,16 @@
 import logo from '$lib/assets/logo.png'
 import login_bg from '$lib/assets/admin/login-bg.png'
-
+import bg_1 from '$lib/assets/admin/bg_1.png'
+import bg_2 from '$lib/assets/admin/bg_2.png'
+import pc_screen from '$lib/assets/admin/pc_screen.png'
 export const AppLogo = logo;
 
 export const AdminLogin = {
   "Background": login_bg
 }
+
+export const MainScreen ={
+  "pc_screen": pc_screen,
+  "Background": bg_1
+}
+

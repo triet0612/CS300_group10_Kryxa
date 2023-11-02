04d9ee2b4e16bb9fd025f0553ea9cfe7dfff469d
LucidNg <94208867+LucidNg@users.noreply.github.com>
2023-11-02 21:05:16 +0700

    addPc button (just UI)

--- src/KryxaWeb/src/lib/Assets.js
+++ src/KryxaWeb/src/lib/Assets.js
@@ -1,8 +1,9 @@
 import logo from '$lib/assets/logo.png'
 import login_bg from '$lib/assets/admin/login-bg.png'
 import bg_1 from '$lib/assets/admin/bg_1.png'
-import bg_2 from '$lib/assets/admin/bg_2.png'
 import pc_screen from '$lib/assets/admin/pc_screen.png'
+import addPc from '$lib/assets/admin/addPc.png'
+
 export const AppLogo = logo;
 
 export const AdminLogin = {
@@ -11,6 +12,7 @@ export const AdminLogin = {
 
 export const MainScreen ={
   "pc_screen": pc_screen,
-  "Background": bg_1
+  "Background": bg_1,
+  "addPc": addPc,
 }
 
--- src/KryxaWeb/src/routes/admin/+page.svelte
+++ src/KryxaWeb/src/routes/admin/+page.svelte
@@ -23,13 +23,24 @@
   <div class="flex flex-col">
     <AdminNav />
   </div>
-  <div class="mr-auto ml-auto p-20 overflow-y-scroll ">
+  <div class="mr-auto ml-auto p-20 overflow-y-scroll">
     <ul class="grid grid-cols-5 gap-20">
+      <div
+        class="w-56 h-56 bg-yellow-400 bg-opacity-70 backdrop-blur-2xl backdrop-brightness-200 text-black-400 font-NotoSans font-bold text-center flex flex-col rounded-xl items-center justify-center"
+      >
+        <div class="h-fit pt-5 px-5">
+          <img src={MainScreen["addPc"]} alt="screen" class="" />
+        </div>
+        <div class="mb-6">Add Pc</div>
+      </div>
       {#each pc_ids as pc_id}
         <li>
           <div
-            class="w-fit h-fit {pc_id['Status'] == 'Available' ? 'bg-green-600 bg-opacity-75'
-              : pc_id['Status'] == 'Unavailable'? 'bg-red-900 bg-opacity-75': 'bg-white  bg-opacity-25' }
+            class="w-56 h-56 {pc_id['Status'] == 'Available'
+              ? 'bg-green-600 bg-opacity-75'
+              : pc_id['Status'] == 'Unavailable'
+              ? 'bg-red-900 bg-opacity-75'
+              : 'bg-white  bg-opacity-25'}
               backdrop-blur-2xl backdrop-brightness-200 text-yellow-400 font-NotoSans text-center flex flex-col rounded-xl items-center justify-center"
           >
             <div class="h-fit pt-5 px-5">

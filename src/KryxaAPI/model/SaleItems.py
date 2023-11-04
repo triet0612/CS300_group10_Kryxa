7b5ea0d88871750c78f9e569c2d5eb331fc23bbf
Tr1ck4 <91774874+Tr1ck4@users.noreply.github.com>
2023-11-04 23:57:50 +0700

    return mapping items

--- src/KryxaAPI/model/SaleItems.py
+++ src/KryxaAPI/model/SaleItems.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field
-from typing import Annotated, Literal
+from typing import Annotated, Literal, List, Dict, Any
 from db.database import DBService
 
 
@@ -8,17 +8,30 @@ class SaleItems(BaseModel):
     Name: Annotated[str, Field(max_length=20)]
     Price: float
     Category: Annotated[str, Field(max_length=20)]
-    ItemStatus: Literal['Deprecated', 'On Sale']
+    ItemStatus: Literal['Deprecated', 'On sale']
     Stock: Annotated[int, Field(ge=0)]
 
 
 def fetch_all_items():
     with DBService() as cur:
-        items = cur.cursor().execute("SELECT * FROM SaleItem").fetchall()
-        return items
+        try:
+            list_items = []
+            items = cur.cursor().execute("SELECT * FROM SaleItem").fetchall()
+            for x in items:
+                list_items.append(
+                    SaleItems(ItemID=x[0], Name=x[1], Price=x[2], Category=x[3], ItemStatus=x[4], Stock=x[5]))
+            print(list_items)
+            return list_items
+        except Exception as err:
+            print(err)
 
 
 def fetch_items_id(itemid):
     with DBService() as cur:
-        items = cur.cursor().execute("SELECT * FROM SaleItem where ItemID = ?", (itemid,)).fetchall()
-        return items
+        try:
+            items = cur.cursor().execute("SELECT * FROM SaleItem where ItemID = ?", (itemid,)).fetchone()
+            fetched = SaleItems(ItemID=itemid, Name=items[1], Price=items[2], Category=items[3], ItemStatus=items[4],
+                                Stock=items[5])
+            return fetched
+        except Exception as err:
+            print(err)

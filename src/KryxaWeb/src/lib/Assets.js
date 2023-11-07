29bdfde90c2da5d06d4c0162c31271562942f358
LucidNg <94208867+LucidNg@users.noreply.github.com>
2023-11-07 16:55:31 +0700

    add bg

--- src/KryxaWeb/src/lib/Assets.js
+++ src/KryxaWeb/src/lib/Assets.js
@@ -1,6 +1,7 @@
 import logo from '$lib/assets/logo.png'
 import login_bg from '$lib/assets/admin/login-bg.png'
 import bg_1 from '$lib/assets/admin/bg_1.png'
+import bg_3 from '$lib/assets/admin/bg_3.png'
 import pc_screen from '$lib/assets/admin/pc_screen.png'
 import addPc from '$lib/assets/admin/addPc.png'
 import banner from '$lib/assets/banner.png'
@@ -16,6 +17,7 @@ export const MainScreen ={
   "pc_screen": pc_screen,
   "Background": bg_1,
   "addPc": addPc,
+  "Background2": bg_3,
 }
 
 export const PcInfo = {

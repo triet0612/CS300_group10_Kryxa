12e4a5c11e35faf1ff3f70f3d87bf7c89e0b8cea
Tr1ck4 <91774874+Tr1ck4@users.noreply.github.com>
2023-11-13 23:23:27 +0700

    Done search

--- src/KryxaWeb/src/lib/Item.js
+++ src/KryxaWeb/src/lib/Item.js
@@ -1,6 +1,6 @@
 export class Item {
-    constructor(ID, Name,Price,Category,ItemStatus,Stock) {
-      this.ID = ID
+    constructor(ItemID, Name,Price,Category,ItemStatus,Stock) {
+      this.ItemID = ItemID
       this.Name = Name
       this.Price = Price
       this.Category = Category
@@ -24,6 +24,31 @@ export class Item {
           console.log(item)
           return item
     }
+
+    async createItem() {
+        let url = "http://localhost:8000/api/admin/items"
+        let statcode = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify({
+            "ItemID": this.ItemID,
+            "Name": this.Name,
+            "Price": this.Price,
+            "Category": this.Category,
+            "ItemStatus": "On sale",
+            "Stock": this.Stock,
+            }),
+            headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + localStorage.getItem("jwt")
+            },
+        })
+        .then(res => res.status)
+        .catch(err => {
+            console.log(err)
+            return 500
+        })
+        return statcode
+    }
 }
 
 export async function fetch_all(){
@@ -41,8 +66,21 @@ export async function fetch_all(){
     console.log(items[0])
     return items
 }
-export async function fetch_category(category){
-    let url = 'http://localhost:8000/api/admin/items?item_category='+category
+
+export async function fetch_category(category,name){
+    let url = 'http://localhost:8000/api/admin/items'
+
+    if(name!==""){
+        url+='?item_name='+name
+        if(category!="All"){
+            url+= '&item_category='+category
+        }
+    }
+    else{
+        if(category!="All"){
+            url+= '?item_category='+category
+        }
+    }
     console.log(url)
     let items = await fetch(url, {
     method: "GET",
@@ -56,5 +94,47 @@ export async function fetch_category(category){
     .catch(err => {console.log(err); return [];})
     console.log(items[0])
     return items
+}
+
+export async function createImage(filename) {
+    let url = "http://localhost:8000/api/admin/uploadfile/"
+    let statcode = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+        "filename": filename
+        }),
+        headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("jwt")
+        },
+    })
+    .then(res => res.status)
+    .catch(err => {
+        console.log(err)
+        return 500
+    })
+    return statcode
+}
+
+export async function loadImage(imgname) {
+    let url = "http://localhost:8000/api/admin/getfile/"
+    if(imgname !==undefined){
+        url+=imgname
+    }
+    let str = await fetch(url, {
+        method: "GET",
+        body: JSON.stringify({
+        "filename": filename
+        }),
+        headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("jwt")
+        },
+    })
+    .then(res => res.json())
+    .then(res => res)
+    .catch(err => {console.log(err); return [];})
+    console.log(str)
+    return str
 }
 
--- src/KryxaWeb/src/routes/admin/items/+page.svelte
+++ src/KryxaWeb/src/routes/admin/items/+page.svelte
@@ -1,78 +1,88 @@
 <script>
-    import AdminNav from "$lib/components/AdminNav.svelte";
-    import {ImageItem} from '$lib/Assets.js';
-    import {Item, fetch_all,fetch_category} from '$lib/Item.js';
-    import { onMount } from "svelte";
-    import {MainScreen} from '$lib/Assets.js';
-    let list_items = []
-    onMount(async () => {
-        list_items = await fetch_all().then(res => res);
-        console.log(list_items);
-    })
-    
-    async function search_al(){
-      var x = document.getElementById("image");
-      var na = x.value.toUpperCase();
-      var y = document.getElementById("search")
-      var us = y.value.toUpperCase();
+  import AdminNav from "$lib/components/AdminNav.svelte";
+  import {ImageItem} from '$lib/Assets.js';
+  import {fetch_all,fetch_category} from '$lib/Item.js';
+  import { onMount } from "svelte";
+  import {MainScreen} from '$lib/Assets.js';
+  import ModalItem from "$lib/components/ModalItem.svelte";
 
-      console.log(na)
+  let list_items = []
+  let text_input = ""
+  let category_filter = "All"
+  let status = "close";
 
+  // pop up
+  function close(event) {
+    if (event.key === "Escape") {
+      status = "close";
     }
+  }
+  function openModal(event) {
+    status = "open";
+  }
+  //Auto pull db
+  onMount(async () => {
+      list_items = await fetch_all().then(res => res);
+      console.log(list_items);
+  })
+  // pull data on request
+  async function search_al(){
+    list_items = await fetch_category(category_filter,text_input)
+    console.log(list_items)
+  }
 
-    async function filter(){
-      var x = document.getElementById("category");
-      var cate = x.value;
-      switch(cate){
-        case 'All':
-          list_items = await fetch_all().then(res=>res);
-          console.log(list_items);
-          break;
-        case 'Drink':
-          list_items = await fetch_category('Drink').then(res=>res);
-          console.log(list_items);
-          break;
-        case 'Food':
-          list_items = await fetch_category('Food').then(res=>res);
-          console.log(list_items);
-          break;
-        case 'Time':
-          list_items = await fetch_category('Time').then(res=>res);
-          console.log(list_items);
-          break;
-      }
-    }
-
-    
 </script>
 
-<div id="bg"class=" fixed h-full w-full bg-cover"style="background-image: url({MainScreen['Background2']});">
+<div id="bg"
+class=" fixed h-full w-full bg-cover {status === 'open' ? 'blur-3xl' : ''} "
+style="background-image: url({MainScreen['Background2']});">
+  <!-- Nav Bar -->
     <div class="fixed">
         <AdminNav/>
     </div>
+    <!-- View Page -->
     <div class="flex flex-col ml-72 h-screen">
+      <!-- Search bar -->
       <div class="fixed z-10">
         <div class="flex flex-row">
-          <input type="text" id="search" placeholder="Seach items..." class="mt-5 px-5 py-5 rounded-3xl" value = ""/>
-          <select name="category" id="category" class="mt-5 ml-5 px-5 py-5 rounded-3xl" on:change={filter}>
-            <option value="All" selected>All</option>
-            <option value="Time" >Time</option>
-            <option value="Food" >Food</option>
-            <option value="Drink" >Drink</option>
+          <!-- Input type check -->
+          <input 
+            type="text" 
+            id="search" 
+            placeholder="Search items..." 
+            class="mt-5 px-5 py-5 rounded-3xl" 
+            bind:value = {text_input} 
+            on:change={search_al}
+          />
+          <!-- Drop down -->
+          <select name="category" id="category" class="mt-5 ml-5 px-5 py-5 rounded-3xl" bind:value={category_filter} on:change={search_al}>
+            <option category_filter="All" selected>All</option>
+            <option category_filter="Time" >Time</option>
+            <option category_filter="Food" >Food</option>
+            <option category_filter="Drink" >Drink</option>
           </select> 
         </div>
       </div>
+      <!-- End search bar -->
       <div class="mt-32 pl-5 overflow-y-scroll">
         <ul class="grid grid-cols-5 gap-5">
+          <div class=" w-56 h-56">
+            <button on:click={openModal} class="w-56 h-48">
+              <img src={MainScreen["addItem"]} alt="cover" />
+            </button>
+            <div class="">Add Item</div>
+          </div>
           {#each list_items as list_item}
             <li class="w-56 h-56 flex-row text-white justify-center text-center border-2 items-center backdrop-blur-2xl">
-              <div class="w-52 h-48 bg-gray-600 ml-1.5"  >
-                <img src = {
-                  ImageItem[list_item.Name]}
-                  alt="screen"
-                  class="bg-contain w-56 h-48 justify-center"s
-                  />
-              </div>
+              <a href="/admin/items/detail?item_id={list_item.ItemID}">
+                <div class="min-w-52 min-h-48 bg-gray-600 ml-1.5">
+                  <img src = {
+                    ImageItem[list_item.Name]}
+                    alt="screen"
+                    class="bg-contain w-56 h-48 justify-center"
+                    />
+                </div>
+              </a>
               <p id ="image"class="justify-center" value={list_item.Name}>{list_item.Name} : {list_item.Price}</p>
             </li>
           {/each}
@@ -80,3 +90,8 @@
       </div>
     </div>
   </div>
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div on:keydown={close}>
+  <ModalItem {status} on:keydown={close} />
+</div>

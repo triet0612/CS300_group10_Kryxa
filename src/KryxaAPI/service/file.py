a0432dff954181b4136c27dadea7a52d6500c9e9
Tr1ck4 <91774874+Tr1ck4@users.noreply.github.com>
2023-11-13 23:23:02 +0700

    import call iamge

--- src/KryxaAPI/service/file.py
+++ src/KryxaAPI/service/file.py
@@ -5,7 +5,7 @@ from PIL import Image
 # Path for resources is at bin/res, create folder before proceed
 class FileManager:
     def __init__(self):
-        self.path = "./bin/res/"
+        self.path = "./bin/system/"
 
     def read_image(self, item_id: int):
         img = Image.open(self.path + str(item_id) + ".png")
@@ -16,7 +16,7 @@ class FileManager:
     def create_image(self, item_id: id, filestream: bytes):
         img = Image.open(io.BytesIO(filestream))
         img.convert("RGBA")
-        img.save(self.path + str(item_id) + ".png")
+        img.save(self.path + str(item_id))
 
 
 def get_file() -> FileManager:

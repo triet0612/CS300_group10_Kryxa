23c27a3963b6f29dc41b6bc119c8559635860f0a
duckhiem2003 <100983194+duckhiem2003@users.noreply.github.com>
2023-11-19 18:33:06 +0700

    modify UI for pc-details

--- src/KryxaWeb/src/routes/admin/pc_info/+page.svelte
+++ src/KryxaWeb/src/routes/admin/pc_info/+page.svelte
@@ -4,12 +4,18 @@
     import {Pc} from "$lib/Pc.js";
     import {updateThisPcByID} from "$lib/Pc.js";
     import { onMount } from "svelte";
-    
     let pc_info = new Pc()
+    let newTime = "newTime"
+    const d = new Date();
+    let real_time = d.toISOString();
     onMount(async()=>{
         const urlSearchParams = new URLSearchParams(window.location.search).get("pc_id");
         console.log("Requested PC ID: "+urlSearchParams)
         pc_info = await pc_info.getPcByID(urlSearchParams).then(res=>res)
+
+        const dateTimeArray = pc_info.EndTime.split("T")
+        const dateArray = dateTimeArray[0].split("-")
+        newTime = dateArray[2]+"/"+dateArray[1]+"/"+dateArray[0]+"  "+dateTimeArray[1]
     })
 
     async function click() {
@@ -24,6 +30,7 @@
     }
 
 
+
 </script>
 
 <div 
@@ -32,58 +39,74 @@
     <div class="flex flex-col">
         <AdminNav />
     </div>
-    <div clas="flex flex-col">
-        <div class = "bg-white/5 backdrop-blur-md rounded-lg mt-40 ml-40 m-auto w-full h-3/5">
+    <div clas="flex flex-col ">
+        <div class = "bg-white/5 backdrop-blur-md rounded-lg mt-40 ml-40 m-auto w-full h-3/5 text-4xl ">
             <div class="flex flex-row">
-                <div class = "basis-3/5 gap-8 grid ml-10 text-amber-700 text-3xl " >
+                <div class = "basis-3/5 grid ml-10 text-amber-700 x" >
 
-                    <div class ="mt-10 h-12 bg-violet-900/50 rounded font-BlackOpsOne"  >
-                        {pc_info.PcID}
-    
+                    <div class = " flex flex-row mt-10 ">
+                        <div class ="w-1/2 flex items-center  h-12 bg-violet-900/50 rounded font-BlackOpsOne"  >
+                            PC ID: {pc_info.PcID}
+                        </div>
+                        <input class ="ml-10 w-1/2 h-12  bg-violet-900/50 rounded font-BlackOpsOne" 
+                            placeholder={pc_info.Password}
+                            bind:value = {pc_info.Password}
+                        >
                     </div>
-                    <div class ="h-12  bg-violet-900/50 rounded font-BlackOpsOne">
-                        {pc_info.EndTime}
-                    </div>
-                    <input class ="h-12  bg-violet-900/50 rounded font-BlackOpsOne" 
-                        placeholder={pc_info.Password}
-                        bind:value = {pc_info.Password}
-                    >
+                    
                     <input class ="h-12  bg-violet-900/50 rounded font-BlackOpsOne" 
                         placeholder={pc_info.IPv4}
                         bind:value={pc_info.IPv4}
                     >
+                    <div class ="flex items-center h-12  bg-violet-900/50 rounded font-BlackOpsOne">
+                        {newTime}
+                    </div>
+                    
+                    
 
                 
                     
                     <div class ="flex flex-row">
-                        <input class ="w-10 h-12 bg-violet-900/50 rounded font-BlackOpsOne" type ="number">
-                        <div class ="ml-3 text-amber-300 font-BlackOpsOne">
-                            x30m    
-                        </div>
+                        <button class = " h-12 w-2/3 h-10 rounded-lg hover:bg-amber-600 active:bg-black  bg-violet-900/50 text-amber-700 font-BlackOpsOne">
+                            Bill
+                        </button>
                         <button on:click={async () => click()}
                             class = "ml-10  h-12 w-2/3 h-10 rounded-lg hover:bg-amber-600 active:bg-black  bg-violet-900/50 text-amber-700 font-BlackOpsOne">
                             Save
+                        </button>
+                        <button class = "ml-10  h-12 w-2/3 h-10 rounded-lg hover:bg-amber-600 active:bg-black  bg-violet-900/50 text-amber-700 font-BlackOpsOne">
+                            Terminate
                         </button>
                     </div>
                 </div>
                 <div class="flex flex-col basis-2/5 text-amber-700 text-3xl">
                     <div class ="">
-                        <button class = "ml-10 mt-10 h-12 w-2/3 rounded-lg hover:bg-amber-600 active:bg-black  bg-violet-900/50 font-BlackOpsOne">
-                            Bill
+                        <button class = "ml-16 mt-10 h-12 w-2/3 rounded-lg hover:bg-amber-600 active:bg-black  bg-violet-900/50 font-BlackOpsOne">
+                            Open
                         </button>
                     </div>
                     
-                    <div class="w-1/2 ml-16">
-                        <img
+                    <div class="mt-5 w-2/3 ml-16 {real_time > pc_info.EndTime
+                        ? 'bg-green-600 bg-opacity-75'
+                        : real_time < pc_info.EndTime
+                        ? 'bg-red-600 bg-opacity-75'
+                        : 'bg-white  bg-opacity-25'}
+                        rounded-xl">
+                        <img 
                           src={MainScreen["pc_screen"]}
                           alt="screen"
-                          class="bg-contain"
+                          class="bg-contain w-11/12 m-auto"
                         />
                     </div>
 
-                    <button class = "ml-10  h-10 w-2/3 h-10 rounded-lg hover:bg-amber-600 active:bg-black  bg-violet-900/50 text-amber-700 font-BlackOpsOne">
-                        Open
-                    </button>
+                    
+                    <div class = "ml-10 flex flex-row">
+                        <input class ="text-center mt-5 ml-10 w-24 h-12 bg-violet-900/50 rounded font-BlackOpsOne" type ="number">
+                        <div class ="mt-7 ml-3 text-amber-300 font-BlackOpsOne">
+                            x30m    
+                        </div>
+                    </div>
+                    
                 </div>
                 
             </div>

8041deb7d3e80aca6e2c7e4df17f040794ebfde4
BuiVuBaoMinh <buivubaominhlqd@gmail.com>
2023-11-22 04:36:02 +0700

    Redo "Update view item detail UI"
    This redo commit 7d2c5e58bb8710b80289bb59be899f75688c1d9f.

--- src/KryxaWeb/src/routes/admin/items/detail/+page.svelte
+++ src/KryxaWeb/src/routes/admin/items/detail/+page.svelte
@@ -14,101 +14,116 @@
     })
 
     async function update_current_item(){
-        await update_item(item).then(res => res)
-        if (files != undefined)
-            await createImage(files, item.ItemID).then(res => res)
+        let upd_statcode = await update_item(item).then(res => res)
+        let upd_img_statcode = await createImage(files, item.ItemID).then(res => res)
+
+        if (upd_statcode != 200){
+            console.log("Item update failed")
+        }
+
+        if (upd_img_statcode != 200){
+            console.log("Item image update failed")
+        }
+
         console.log(item)
-        window.location.reload()
+        location.reload()
     }
 
     async function delete_current_item(){
         let del_statcode = await delete_item(item.ItemID).then(res => res)
+        
         if (del_statcode != 200){
             console.log("Item deletion failed")
         }
+
         console.log("Delete item called")
-        // window.location.replace("/admin/items")
+        window.location.replace("/admin/items")
     } 
 
 </script>
 
 <div 
-    class="flex h-full w-full bg-cover bg-fixed"
+    class="flex flex-row h-full w-full bg-cover bg-fixed"
     style="background-image: url({MainScreen['Background2']});">
     <div class="w-fit">
         <AdminNav></AdminNav>
     </div>
-    <div class="w-fit h-3/4 place-self-center p-16 ml-12 border-4 bg-gradient-to-b from-[#342267] black">
-        <div class="w-fit h-fit grid grid-cols-3">
-            <div class="w-full h-fit flex flex-col gap-y-3 col-span-2">
-                <label class="grid grid-flow-row h-fit w-full pr-4">
-                    <span class="text-slate-200 text-3xl">
-                        Name
-                    </span>
-                    <input 
-                        type="text" 
-                        bind:value={item.Name}
-                        class="h-fit w-full focus:{"border-2 border-[#facc15] outline-amber-500"} rounded-lg text-2xl text-[#BA7000] whitespace-nowrap"/>
-                </label>
-                <div class="w-fit h-fit grid grid-flow-col">
-                    <div class="w-[255px] h-fit grid grid-flow-row gap-y-4">
-                        <!-- svelte-ignore a11y-img-redundant-alt -->
-                        <img src="http://localhost:8000/api/admin/getfile/{item.ItemID}" alt="Item Image" class="w-[255px] h-[255px]">
-                        <input bind:files class="w-full h-fit my-1 mx-1 bg-white rounded-xl text-left" type="file" accept="image/png, image/jpeg">
+    <div class="w-full h-screen flex">
+        <div class="w-fit h-fit mx-auto place-self-center p-16 border-2 rounded-lg {"bg-gradient-to-b from-[#342267] to-black"}/30">
+            <div class="w-fit h-fit grid grid-cols-3">
+                <div class="w-full h-fit flex flex-col gap-y-3 col-span-2">
+                    <label class="grid grid-flow-row h-fit w-full pr-4">
+                        <input 
+                            type="text" 
+                            placeholder="Name"
+                            bind:value={item.Name}
+                            class="h-fit w-full bg-[#160425]/75 rounded-lg whitespace-nowrap
+                            indent-2.5 text-4xl text-[#BA7000] leading-relaxed
+                            placeholder:italic placeholder:text-[#BA7000]
+                            focus:outline-none focus:ring-2 focus:ring-[#BA7000]"/>
+                    </label>
+                    <div class="w-fit h-fit grid grid-flow-col">
+                        <div class="w-[255px] h-fit grid grid-flow-row gap-y-4">
+                            <!-- svelte-ignore a11y-img-redundant-alt -->
+                            <img src="http://localhost:8000/api/admin/getfile/{item.ItemID}" alt="Item Image" class="w-[255px] h-[255px]">
+                            <input bind:files class="w-full h-fit my-1 mx-1 bg-white rounded-xl text-left" type="file" accept="image/png, image/jpeg">
+                        </div>
+                        <div class="h-fit grid grid-flow-row gap-y-10 mx-4">
+                            <label class="grid grid-flow-row h-fit w-full">
+                                <input 
+                                    type="number"
+                                    placeholder="Price"
+                                    bind:value={item.Price}
+                                    class="h-fit w-full bg-[#160425]/75 rounded-lg whitespace-nowrap
+                                    indent-2.5 text-3xl text-[#BA7000] leading-relaxed
+                                    placeholder:italic placeholder:text-[#BA7000]
+                                    focus:outline-none focus:ring-2 focus:ring-[#BA7000]"/>
+                            </label>
+            
+                            <label class="grid grid-flow-row h-fit w-full">
+                                <select class="h-fit w-full bg-[#160425]/75 rounded-lg whitespace-nowrap
+                                indent-2.5 text-3xl text-[#BA7000] leading-relaxed
+                                placeholder:italic placeholder:text-[#BA7000]
+                                focus:outline-none focus:ring-2 focus:ring-[#BA7000]"
+                                 bind:value={item.Category}>
+                                    <option item.Category="Time" class="text-3xl focus:outline-none focus:ring-2 focus:ring-[#BA7000]" >Time</option>
+                                    <option item.Category="Food" class="text-3xl focus:outline-none focus:ring-2 focus:ring-[#BA7000]">Food</option>
+                                    <option item.Category="Drink" class="text-3xl focus:outline-none focus:ring-2 focus:ring-[#BA7000]">Drink</option>
+                                </select>
+                            </label>
+            
+                            <label class="grid grid-flow-row h-fit w-full">
+                                <input 
+                                    type="number"
+                                    placeholder="Stock"
+                                    bind:value={item.Stock}
+                                    class="h-fit w-full bg-[#160425]/75 rounded-lg whitespace-nowrap
+                                    indent-2.5 text-3xl text-[#BA7000] leading-relaxed
+                                    placeholder:italic placeholder:text-[#BA7000]
+                                    focus:outline-none focus:ring-2 focus:ring-[#BA7000]"/>
+                            </label>
+                        </div>
                     </div>
-                    <div class="h-fit grid grid-flow-row gap-y-4 mx-4">
-                        <label class="grid grid-flow-row h-fit w-full">
-                            <span class="text-slate-200 text-3xl">
-                                Price
-                            </span>
-                            <input 
-                                type="number" 
-                                bind:value={item.Price}
-                                class="h-fit w-full focus:{"border-2 border-[#facc15] outline-amber-500"} rounded-lg text-2xl text-[#BA7000] whitespace-nowrap"/>
-                        </label>
-        
-                        <label class="grid grid-flow-row h-fit w-full">
-                            <span class="text-slate-200 text-3xl">
-                                Category
-                            </span>
-                            <select class="h-full w-full focus:{"border-2 border-[#facc15] outline-amber-500"} rounded-lg text-2xl text-[#BA7000]" bind:value={item.Category}>
-                                <option item.Category="Time" >Time</option>
-                                <option item.Category="Food" >Food</option>
-                                <option item.Category="Drink" >Drink</option>
-                            </select>
-                        </label>
-        
-                        <label class="grid grid-flow-row h-fit w-full">
-                            <span class="text-slate-200 text-3xl">
-                                Stock
-                            </span>
-                            <input 
-                                type="number"
-                                bind:value={item.Stock}
-                                class="h-fit w-full focus:{"border-2 border-[#facc15] outline-amber-500"} rounded-lg text-2xl text-[#BA7000] whitespace-nowrap"/>
-                        </label>
-                    </div>
+                    
                 </div>
-                
-            </div>
-            <div class="flex flex-col h-fit w-full mx-14 px-4 gap-y-3 place-self-center">
-                <button class=" h-fit w-full bg-white 
-                            ring ring-green-500
-                            hover:{"bg-gradient-to-r from-green-500 to-cyan-500"}
-                            text-2xl text-green-500
-                            hover:text-white" 
-                            on:click={update_current_item}>
-                    Edit
-                </button>
-                <button class="h-fit w-full bg-white 
-                            ring ring-red-500
-                            hover:{"bg-gradient-to-r from-red-500 to-pink-500 text-white"}
-                            text-2xl text-red-500
-                            hover:text-white"
-                            on:click={delete_current_item}>
-                    Delete
-                </button>
+                <div class="flex flex-col h-fit w-full mx-14 px-4 gap-y-3 place-self-center">
+                    <button class=" h-fit w-full {"bg-gradient-to-b from-[#160425] to-black"}/75
+                                rounded-lg
+                                text-3xl text-[#BA7000] leading-relaxed
+                                hover:bg-gradient-to-b hover:from-[#160425] hover:to-cyan-500/75" 
+                                on:click={update_current_item}>
+                        Edit
+                    </button>
+                    <button class=" h-fit w-full {"bg-gradient-to-b from-[#160425] to-black"}/75
+                                rounded-lg
+                                text-3xl text-[#BA7000] leading-relaxed
+                                hover:bg-gradient-to-b hover:from-[#160425] hover:to-pink-500/75" 
+                                on:click={delete_current_item}>
+                        Delete
+                    </button>
+                </div>
             </div>
         </div>
     </div>
+    
 </div>
